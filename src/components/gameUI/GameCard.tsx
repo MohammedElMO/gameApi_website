b0a0a48b1feccb42f832e84b7214d656faf9d8dd
mohamedElC <amranimohamad040@gmail.com>
2023-04-13 17:37:04 +0000

    fixing responsivness for game cards

--- src/components/gameUI/GameCard.tsx
+++ src/components/gameUI/GameCard.tsx
@@ -16,17 +16,17 @@ const GameCard = ({ game, isLoading }: GameT) => {
         <>
             
             {game.map((gamer) => 
-                <article key={gamer.id} className="flex flex-col shadow-lg  items-start overflow-hidden bg-[#232223] hover:-translate-y-1.5 transition-all rounded-2xl font-pop cursor-pointer  ">
+                <article key={gamer.id} className="flex flex-col shadow-lg  items-start overflow-hidden bg-[#232223] hover:-translate-y-1.5 transition-all rounded-2xl font-pop cursor-pointer max-h-fit h-max ">
                     {isLoading && <GameSkeleton boxs={gamer.parent_platforms.map(plat => plat.platform)} /> }       
                     <figure >
                         <img className=" object-contain" src={CropImage(gamer.background_image)} alt="cyber gamer picture" />
                     </figure>
                     <div className="w-full flex p-4 gap-2  font-semibold text-white flex-col px-4">
-                        <h3 className="text-2xl max-sm:text-2xl max-md:text-3xl ">{gamer.name}</h3>
                         <div className="flex flex-row-reverse py-3 items-end justify-between">
                             <GameBadge metacritic={gamer.metacritic} />
                             <GameIcons platforms={gamer.parent_platforms.map(plat => plat.platform)} />
                         </div>
+                        <h3 className="text-2xl max-sm:text-2xl max-md:text-3xl ">{gamer.name}</h3>
                         <div className="bg-slate-700 p-2 rounded">
                             {gamer.rating}
                         </div>
--- src/components/gameUI/GamesGrid.tsx
+++ src/components/gameUI/GamesGrid.tsx
@@ -3,11 +3,11 @@ import { useRequestedGames } from "../hooks/useRequestedGame";
 import { Genre } from "../hooks/useRequestedGenres";
 import { Platform } from "../hooks/useRequestedPlatforms";
 import { GameQuery } from "../../App";
-interface Props  {
+interface Props {
     gameQuery: GameQuery
 }
 
-const GamesGrid = ({gameQuery}:Props) => {
+const GamesGrid = ({ gameQuery }: Props) => {
     const { data, isLoading } = useRequestedGames(gameQuery)
 
 
@@ -16,17 +16,18 @@ const GamesGrid = ({gameQuery}:Props) => {
     return (
         <>
             <div className="
-            lg:grid-cols-3
-            max-semi-md:grig-cols-1
-            max-sm:grid-cols-1
-            max-md:grid-cols-2
-            grid-cols-3
-            grid 
-            p-5
-            grid-rows-2
-            gap-4">
+                    grid 
+                    lg:grid-cols-3
+                    xl:grid-cols-3
+                    max-semi-md:grig-cols-1
+                    max-sm:grid-cols-1
+                    max-md:grid-cols-2
+                    grid-cols-3
+                    p-5
+                    grid-rows-2
+                    gap-5"
+            >
                 <GameCard game={data} isLoading={isLoading} />
-
             </div>
 
         </>
