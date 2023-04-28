6e5eea8b01f1c957b9798b5471475e2f5cf81098
MohammedElMO <elamranimohammed104@gmail.com>
2023-04-28 22:51:26 +0100

    hello that is the first commit from long time

--- src/components/hooks/useRequestedGame.ts
+++ src/components/hooks/useRequestedGame.ts
@@ -1,7 +1,7 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { create } from "../services/HttpClient";
+import { DataApi } from '../services/api-client';
 import type { GameQuery } from './../../App';
-import { useQuery } from '@tanstack/react-query';
-import baseApi, { DataApi } from '../services/api-client'
-import {create} from "../services/HttpClient"
 
 export interface Platform   {
     id: number
@@ -23,16 +23,19 @@ export interface GameResponse {
 }
 const useRequestedGames = (GameQuery:GameQuery) => {
 
-   return  useQuery<DataApi<GameResponse>,Error>({
+   return  useInfiniteQuery<DataApi<GameResponse>,Error>({
         queryKey:["games",GameQuery],
-        queryFn: () => create("/games").getAll({ 
+        queryFn: ({pageParam = 1}) => create("/games").getAll({ 
             params:{
             genres: GameQuery.genre?.id,
             parent_platforms: GameQuery.platform?.id,
             ordering: GameQuery.sortBy,
-            search:GameQuery.searchgame
-        
-    }})
+            search:GameQuery.searchgame,
+            page:pageParam
+    }}),
+    getNextPageParam:(lastPage,allPages) => {return lastPage?.next ? allPages.length++ : lastPage}
+
+    
 
     })
 
